767deb81db9a4362f7dc376383eb7f0849b6faa8
Vkrasnovetskyi <valeriykrasnoveckii@gmail.com>
2023-04-30 20:50:46 +0300

    redact models.py

--- products/models.py
+++ products/models.py
@@ -1,7 +1,9 @@
-import uuid
 from os import path
 
 from django.db import models
+from django.core.validators import MinValueValidator
+from project.constants import MAX_DIGITS, DECIMAL_PLACES
+from project.mixins.models import PKMixin
 
 
 def upload_to(instance, filename):
@@ -9,35 +11,43 @@ def upload_to(instance, filename):
     return f'products/images/{str(instance.pk)}{extension}'
 
 
-class Product(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+class Category(PKMixin):
     name = models.CharField(max_length=255)
     description = models.TextField(
         blank=True,
         null=True
     )
-    # image = models.ImageField(upload_to)
+    image = models.ImageField(
+        upload_to=upload_to,
+        null=True,
+        blank=True
+    )
+
+
+class Product(PKMixin):
+    name = models.CharField(max_length=255)
+    description = models.TextField(
+        blank=True,
+        null=True
+    )
+    image = models.ImageField(
+        upload_to=upload_to,
+        null=True,
+        blank=True
+    )
     sku = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-
-class Category(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=255)
-    description = models.TextField(
-        blank=True,
-        null=True
+    categories = models.ManyToManyField(Category, blank=True)
+    products = models.ManyToManyField('products.Product', blank=True)
+    price = models.DecimalField(
+        validators=[MinValueValidator(0)],
+        max_digits=MAX_DIGITS,
+        decimal_places=DECIMAL_PLACES
     )
-    # image = models.ImageField(upload_to)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
 
-class Discount(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    amount = models.PositiveIntegerField(max_length=255)
-    code = models.CharField(max_length=255)
-    is_active = models.BooleanField(default=True)
-    discount_type = models.PositiveIntegerField(max_length=255)
+# class Discount(PKMixin):
+#    amount = models.PositiveIntegerField(max_length=255)
+#    code = models.CharField(max_length=255)
+#    is_active = models.BooleanField(default=True)
+#    discount_type = models.PositiveIntegerField(max_length=255)
