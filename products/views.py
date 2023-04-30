77a6b423951cf9e66ab1256cd3e3e154f11a7d96
Vkrasnovetskyi <valeriykrasnoveckii@gmail.com>
2023-04-30 16:16:41 +0300

    ibdp

--- products/views.py
+++ products/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
 
 def products(request, *args, **kwargs):
+
     return render(request, 'products/index.html')
+
+"""
+export PYTHONBREAKPOINT=ipdb.set.trace
+"""
