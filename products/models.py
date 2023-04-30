6a71131d6010c87dc8bc93ab0d01e4fbfa43d33c
Vkrasnovetskyi <valeriykrasnoveckii@gmail.com>
2023-04-30 19:46:54 +0300

    add Category and  Discount in models and migration

--- products/models.py
+++ products/models.py
@@ -16,8 +16,28 @@ class Product(models.Model):
         blank=True,
         null=True
     )
-    #image = models.ImageField(upload_to)
+    # image = models.ImageField(upload_to)
     sku = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+
+class Category(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255)
+    description = models.TextField(
+        blank=True,
+        null=True
+    )
+    # image = models.ImageField(upload_to)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Discount(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    amount = models.PositiveIntegerField(max_length=255)
+    code = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    discount_type = models.PositiveIntegerField(max_length=255)
